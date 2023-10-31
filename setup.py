d23f806bb1a18935bd1284dbcc825f0394b35f94
RohanSunkarapalli <58287801+RohanSunkarapalli@users.noreply.github.com>
2023-10-31 15:00:51 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ Then, you can use it in your Python code:
 """
 
 # Other information
-VERSION = "0.1.5"
+VERSION = "0.1.6"
 DESCRIPTION = "URL generator tool for National Water Model data"
 
 setup(
